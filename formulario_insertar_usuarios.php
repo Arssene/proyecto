6e2beccc4d3f356e5a8c22d23d957dca7db2d40e
Carlos <kkintas@gmail.com>
2023-11-26 22:42:47 +0100

    Fix merge

--- formulario_insertar_usuarios.php
+++ formulario_insertar_usuarios.php
@@ -42,25 +42,3 @@
 
 </body>
 </html>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-    <form action="pagina_insertar_usuarios.php" method="post">
-
-
-
-
-
-
-
-
-
-</body>
-</html>
