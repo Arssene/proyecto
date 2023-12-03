ec37cec585dd3c355ff7fe951a06bfe90fe912f4
Carlos <kkintas@gmail.com>
2023-12-03 10:10:32 +0100

    cambios en formulario contacto

--- apuntatemail.php
+++ apuntatemail.php
@@ -7,7 +7,7 @@
 		$telefono = $_POST["telefono"];
 		$destinatario = "kkintas@gmail.com";
 		$asunto = "Nova mensaxe de " . $nombre;
-		$contenido = "Nome: " . $nombre . "\n Email: " . $email . "\n Telefono: " . $telefono . "\n Mensaxe: " . $mensaxe;
+		$contenido = "Nome: " . $nombre . "\nEmail: " . $email . "\nTelefono: " . $telefono . "\nMensaxe: " . $mensaxe;
 		
 		$header ="from: apuntate@academiaepsilon.com";
 		$mail = mail($destinatario, $asunto,$contenido,$header);
