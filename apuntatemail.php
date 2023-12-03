782d1e89528c79b0126abefcf429cc5fe5395e8d
Carlos <kkintas@gmail.com>
2023-12-03 10:05:54 +0100

    Cambiadop mensaxe de contacto

--- apuntatemail.php
+++ apuntatemail.php
@@ -4,9 +4,10 @@
 		$nombre = $_POST["nombre"];
 		$email = $_POST["email"];
 		$mensaxe = $_POST["mensaxe"];
+		$telefono = $_POST["telefono"];
 		$destinatario = "kkintas@gmail.com";
-		$asunto = "Nuevo mensaje de $nombre";
-		$contenido = $mensaxe;
+		$asunto = "Nova mensaxe de " . $nombre;
+		$contenido = "Nome: " . $nombre . "\n Email: " . $email . "\n Telefono: " . $telefono . "\n Mensaxe: " . $mensaxe;
 		
 		$header ="from: apuntate@academiaepsilon.com";
 		$mail = mail($destinatario, $asunto,$contenido,$header);
